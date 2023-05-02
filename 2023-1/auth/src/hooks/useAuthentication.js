0cd9ca9ffb96e8d80b795a23d84cb68176ce463f
rafybot <rafaelagasaisan@gmail.com>
2023-05-02 10:43:32 -0300

    createUser w/ auth firebase

---
+++ 2023-1/auth/src/hooks/useAuthentication.js
@@ -0,0 +1,50 @@
+import { authentication } from "../firebase/config";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile, signOut } from "firebase/auth";
+import { useState, useEffect } from 'react';
+
+export const useAuthentication = () => {
+    const [error, setError] = useState(null)
+    const [loading, setLoading] = useState(null)
+
+    //cleanup
+    const [cancelled, setCancelled] = useState(false)
+
+    const auth = getAuth()
+
+    function checkIfIsCancelled() {
+        if(cancelled) return;
+    }
+
+    const createUser = async (data) => {
+      checkIfIsCancelled();
+      setLoading(true)
+      setError(null)
+      try {
+        const user = await createUserWithEmailAndPassword(
+            auth,
+            data.email,
+            data.password
+        )
+        await updateProfile(user, {
+            displayName: data.displayName
+        })
+
+        setLoading(false)
+        return user
+
+      } catch (error) {
+        console.log(error.message)
+        console.log(typeof error.message)
+
+        let systemErrorMessage
+        if (error.message.includes("Password")) {
+            systemErrorMessage = "A senha precisa conter pelo menos 6 caracteres!"
+        } else if (error.message.includes("email-already")) {
+            systemErrorMessage = "Este e-mail já está cadastrado no site!"
+        } else {
+            systemErrorMessage = "Ocorreu um erro inesperado! Tente novamente mais tarde."
+        }
+        setError(systemErrorMessage)
+      }
+    }
+}
